4cba181e319961f7f6e9523035f6432f973a2229
aaronhleung <aaronleung@outlook.com>
2022-05-31 06:20:06 -0700

    reupload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Team Profile Generator
+
+A server-side command line interface application that generates an HTML page based on user's input of employee information.
+
+### Usage
+* Run the node index.js command in terminal while in the root directory
+* Answer the command line prompts
+* When the questionaire is completed, an HTML page will be generated in the output folder.
+
+
+### Technologies Used
+HTML, CSS, JavaScript, Node.js, Jest, Inquirer, fs
+
+
---
+++ index.js
@@ -0,0 +1,121 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const employees = [];
+
+
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Please enter employee name:"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "Please enter employee ID number:"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter employee e-mail address:"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is the employee's role in the team?",
+        choices: [{
+            name: "Manager"
+        },
+        {
+            name: "Engineer"
+        },
+        {
+            name: "Intern"
+        }]
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "Please enter manager's office number:",
+        when: function (answers) {
+            return answers.role === "Manager";
+        }
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Please enter engineer's github username:",
+        when: function (answers) {
+            return answers.role === "Engineer";
+        }
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the name of the school you're currently attending?",
+        when: function (answers) {
+            return answers.role === "Intern";
+        }
+    }
+];
+
+function promptUser() {
+
+    inquirer.prompt(questions).then(function (response) {
+        if (response.role === "Manager") {
+            const currentManager = new Manager(response.name, response.id, response.email, response.officeNumber);
+            employees.push(currentManager);
+        }
+        else if (response.role === "Engineer") {
+            const currentEngineer = new Engineer(response.name, response.id, response.email, response.github);
+            employees.push(currentEngineer);
+        }
+        else if (response.role === "Intern") {
+            const currentIntern = new Intern(response.name, response.id, response.email, response.school);
+            employees.push(currentIntern);
+        }
+        currentEmployee();
+    });
+};
+
+function currentEmployee() {
+    inquirer.prompt([
+        {
+            type: "confirm",
+            name: "currentEmployee",
+            message: "Would you like to enter another employee?",
+            default: true
+        }
+    ]).then(function (answers) {
+        if (answers.currentEmployee) {
+            promptUser();
+        }
+        else {
+            const data = render(employees);
+            fs.writeFile(outputPath, data, function (err) {
+                if (err) throw err;
+            })
+        }
+    })
+}
+
+function init() {
+
+    promptUser();
+
+}
+
+init();
+
+
+
